1040b398fce7d3c4a145b33caa4e6f0184143858
Archor <antoine.combe@edu.ece.fr>
2020-04-15 14:43:14 +0200

    Navbar + carousel

--- Ce qu'on a/VerifChampVide.js
+++
@@ -1,8 +0,0 @@
-function testEmpty() {
-    if ((document.getElementById("name").value == "") ||
-        (document.getElementById("age").value == "") ||
-        (document.getElementById("phone").value == "") ||
-        (document.getElementById("birthday").value == "")) {
-        alert("Un champ est vide");
-    }
-}
