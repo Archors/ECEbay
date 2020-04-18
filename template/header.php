3628e7de9b5892b2fb8055c2b3e3d6a9530d6808
EmilienDelahegue <delahegueemilien@yahoo.fr>
2020-04-18 11:52:05 +0200

    footer header

--- template/header.php
+++ template/header.php
@@ -12,30 +12,74 @@
         $(document).ready(function() {
             $('.header').height($(window).height());
         });
+        $(window).on("load resize", function() {
     </script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     <!-- Intégration d'un librairie d'icones -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script type="text/javascript">
+
+    </script>
     <link rel="stylesheet" type="text/css" href="../template/styleshtf.css">
 
 </head>
     
 <body>
-    <nav class="navbar navbar-expand-md">
-     <a class="navbar-brand" href="accueil.php">Logo</a>
-     <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#main-navigation">
+<nav class="navbar navbar-expand">
+        <a class=" navbar-brand " href="# "><img src="../images/Logo.png" alt="Home " height="50" width="auto" /></a>
+        <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
      <span class="navbar-toggler-icon"></span>
      </button>
-     <div class="collapse navbar-collapse" id="main-navigation">
-     <ul class="navbar-nav">
-     <li class="nav-item"><a class="nav-link" href="categorie.php">Catégories</a></li>
-     <li class="nav-item"><a class="nav-link" href="#">Acheter</a></li>
-     <li class="nav-item"><a class="nav-link" href="#">Vendre</a></li>
-     <li class="nav-item"><a class="nav-link" href="#">Admin</a></li>
-     <li class="nav-item"><a class="nav-link" href="#">Compte</a></li>
-     <li class="nav-item"><a class="nav-link" href="#"><i class="fas fa-shopping-cart"></i> Panier</a></li>
-     </ul>
-     </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Catégories
+              </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="../acheter/categorie.php">Férailles et Trésor</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Bon musées</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Accessoires VIP</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Acheter
+              </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Achat immédiat</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Enchère</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Meilleur offre</a>
+                    </div>
+                </li>
+                <li class="nav-item"><a class="nav-link" href="../vendeur/form_vendeur.php">Vendre</a></li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Admin
+              </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="../admin/form_admin.php">Se connecter</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Compte
+              </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">Se connecter</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item">Vous êtes nouveau ?</a>
+                        <a class="dropdown-item" href="#"> S'inscrire  </a>
+                    </div>
+                </li>
+                <li class="nav-item"><a class="nav-link" href="../acheteur/panier.php"><i class="fas fa-shopping-cart"></i> Panier</a></li>
+            </ul>
+        </div>
     </nav>
     
 </body>
