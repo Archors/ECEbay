b8d539d4ed4ca3d5bad4dff11a5fde8d05576064
Archor <antoine.combe@edu.ece.fr>
2020-04-18 12:13:15 +0200

    logo ramene a l'accueil

--- template/header.php
+++ template/header.php
@@ -27,7 +27,7 @@
     
 <body>
 <nav class="navbar navbar-expand">
-        <a class=" navbar-brand " href="# "><img src="../images/Logo.png" alt="Home " height="50" width="auto" /></a>
+        <a class=" navbar-brand " href="../accueil/accueil.php"><img src="../images/Logo.png" alt="Home " height="50" width="auto" /></a>
         <button class="navbar-toggler navbar-dark" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
      <span class="navbar-toggler-icon"></span>
      </button>
